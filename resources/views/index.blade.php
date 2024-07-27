308a2d2540788c61b2c4a26dc2caf7c43b780e89
Anthony Esquilona <91820043+Curlybites@users.noreply.github.com>
2024-07-27 17:39:08 +0800

    home

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,68 @@
 <x-header />
 <x-navbar />
+<div class="container mx-auto px-4">
+
+    <div class="mb-4 mt-5">
+        <a href="#"
+            class="flex flex-col md:flex-row  bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <img class="object-cover w-full md:w-1/3 h-96 md:h-96 rounded-t-lg md:rounded-none md:rounded-l-lg"
+                src="https://media.philstar.com/photos/2023/08/07/ayungin-shoaljpeg_2023-08-07_17-36-17_thumbnail.jpg"
+                alt="">
+            <div class="flex flex-col justify-center p-4 w-full md:w-2/3">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology
+                    acquisitions 2021</h5>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error nostrum accusamus cum laborum consequuntur quo. Magni aliquam asperiores tempora alias. Expedita quos sed facere natus est reiciendis ducimus at aliquid. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam ex repudiandae deleniti ab, atque nulla a veritatis aperiam voluptatum, architecto et doloribus perspiciatis qui pariatur dolorum, deserunt assumenda! Veritatis fugit eligendi officia illum consequatur quae mollitia voluptates nesciunt amet iure.</p>
+            </div>
+        </a>
+    </div>
+
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+
+        <div
+            class="flex flex-col md:flex-row bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <img class="object-cover w-full md:w-1/3 h-32 md:h-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
+                src="https://media.philstar.com/photos/2023/08/07/ayungin-shoaljpeg_2023-08-07_17-36-17_thumbnail.jpg"
+                alt="">
+            <div class="flex flex-col justify-between p-4 w-full md:w-2/3">
+                <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology
+                    acquisitions 2021</h5>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology
+                    acquisitions of 2021 so far, in reverse chronological order.</p>
+            </div>
+        </div>
+
+        <div
+            class="flex flex-col md:flex-row bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <img class="object-cover w-full md:w-1/3 h-32 md:h-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
+                src="https://media.philstar.com/photos/2023/08/07/ayungin-shoaljpeg_2023-08-07_17-36-17_thumbnail.jpg"
+                alt="">
+            <div class="flex flex-col justify-between p-4 w-full md:w-2/3">
+                <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology
+                    acquisitions 2021</h5>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology
+                    acquisitions of 2021 so far, in reverse chronological order.</p>
+            </div>
+        </div>
+
+        <div
+            class="flex flex-col md:flex-row bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <img class="object-cover w-full md:w-1/3 h-32 md:h-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
+                src="https://media.philstar.com/photos/2023/08/07/ayungin-shoaljpeg_2023-08-07_17-36-17_thumbnail.jpg"
+                alt="">
+            <div class="flex flex-col justify-between p-4 w-full md:w-2/3">
+                <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology
+                    acquisitions 2021</h5>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology
+                    acquisitions of 2021 so far, in reverse chronological order.</p>
+            </div>
+        </div>
+
+        
+    </div>
+</div>
+
+
+
+
+
 <x-footer />
